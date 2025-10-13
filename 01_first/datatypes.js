270cbfcb16b48eb2f526d2c0f3709a4454e90bea
Samita1234 <samita.sahoo126@gmail.com>
2025-10-13 16:16:59 +0530

    datatypes

---
+++ 01_first/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //makes your code safer, more reliable, and easier to debug , catches bug and error and reserved keyword
+
+// alert("sam"); // will give you error
+
+
+
+//primitive datatype
+console.log("samita"); //string- "" or ''
+console.log(89); //number
+console.log(null); //null- typeof null is object
+console.log(undefined);  //undefined
+// console.log(symbol); // unique
+console.log(true); //boolean
+
+//object - non-primitive datatype
